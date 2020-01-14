03d757369d9a2d7824ee87e52ffa3cbfb0d9889c
Braulio Camarena <braulio.camarenah@gmail.com>
2020-01-14 08:21:03 -0600

    added sigup and login

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,7 +1,92 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './Auth.css';
 const Auth = () => {
-  return <h1>The Auth Page</h1>;
+  const [values, setValues] = useState({});
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleChange = event => {
+    setValues({
+      ...values,
+      [event.target.type]: event.target.value
+    });
+  };
+  const submitHadler = event => {
+    event.preventDefault();
+    const email = values.email || '';
+    const password = values.password || '';
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}"){
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+
+    if (!isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput : {email: "${email}", password: "${password}"}){
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <form className='auth-form' onSubmit={submitHadler}>
+      <div className='form-control'>
+        <label htmlFor='email'>E-mail</label>
+        <input type='email' id='email' onChange={handleChange} />
+      </div>
+      <div className='form-control'>
+        <label htmlFor='password'>Password</label>
+        <input type='password' id='password' onChange={handleChange} />
+      </div>
+      <div className='form-actions'>
+        <button type='submit'>submit</button>
+        <button
+          type='button'
+          onClick={() => {
+            setIsLogin(!isLogin);
+          }}
+        >
+          Switch to {isLogin ? 'Sigup' : 'Login'}
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Auth;
