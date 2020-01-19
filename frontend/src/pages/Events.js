084932c527dd2bd27e7baabad27015a229d78e2d
Braulio Camarena <braulio.camarenah@gmail.com>
2020-01-18 19:18:14 -0600

    Allow create and fech events

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,15 +1,124 @@
-import React, { useState } from 'react';
-
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from '../context/auth-context';
 import Modal from '../components/Modal/Modal';
 import './Events.css';
 const Events = () => {
+  const context = useContext(AuthContext);
+  const [events, setEvents] = useState([]);
+  const [values, setValues] = useState({});
   const [openCreateModal, setOpenCreateModal] = useState(false);
+
+  const token = context.token;
+
   const handleCancel = () => {
     setOpenCreateModal(false);
   };
+
+  const fetchEvents = () => {
+    const requestBody = {
+      query: `
+          query {
+            events{
+              _id
+              title
+              description
+              date
+              price
+              creator {
+                _id
+                email
+              }
+            }
+          }
+        `
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        setEvents(resData.data.events);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   const handleConfirm = () => {
+    const title = values.title || '';
+    const price = +values.price || '';
+    const date = values.date || '';
+    const description = values.description || '';
+
+    if (
+      title.trim().length === 0 ||
+      price <= 0 ||
+      date.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      return;
+    }
+
+    const requestBody = {
+      query: `
+          mutation {
+            createEvent(eventInput : {title: "${title}", description: "${description}" , price: ${price} , date: "${date}"}){
+              _id
+              title
+              description
+              price
+              creator{
+                _id
+                email
+              }
+            }
+          }
+        `
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        //console.log(resData);
+        fetchEvents();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
     setOpenCreateModal(false);
   };
+
+  const handleChange = event => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  useEffect(fetchEvents, []);
 
   return (
     <>
@@ -21,19 +130,60 @@ const Events = () => {
         onCancel={handleCancel}
         onConfirm={handleConfirm}
       >
-        <p>Content</p>
+        <form>
+          <div className='form-control'>
+            <label htmlFor='title'>Title</label>
+            <input
+              type='text'
+              id='title'
+              name='title'
+              onChange={handleChange}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='price'>Price</label>
+            <input
+              type='number'
+              id='price'
+              name='price'
+              onChange={handleChange}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='date'>Date</label>
+            <input type='date' id='date' name='date' onChange={handleChange} />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='description'>Description</label>
+            <textarea
+              id='description'
+              rows='4'
+              name='description'
+              onChange={handleChange}
+            ></textarea>
+          </div>
+        </form>
       </Modal>
-      <div className='events-control'>
-        <p>Share your own Events</p>
-        <button
-          className='btn'
-          onClick={() => {
-            setOpenCreateModal(true);
-          }}
-        >
-          Create Event
-        </button>
-      </div>
+      {context.token && (
+        <div className='events-control'>
+          <p>Share your own Events</p>
+          <button
+            className='btn'
+            onClick={() => {
+              setOpenCreateModal(true);
+            }}
+          >
+            Create Event
+          </button>
+        </div>
+      )}
+      <ul className='events__list'>
+        {events.map(event => (
+          <li key={event.id} className='events__list-item'>
+            {event.title}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
